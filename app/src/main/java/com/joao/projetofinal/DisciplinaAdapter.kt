425499696b309ec812ca16d6d287427e0bff8141
joaofelipesc <joaocoromberk@gmail.com>
2024-06-05 14:44:10 -0300

    ultimo commit

---
+++ app/src/main/java/com/joao/projetofinal/DisciplinaAdapter.kt
@@ -0,0 +1,4 @@
+package com.joao.projetofinal
+
+class DisciplinaAdapter {
+}
--- app/src/main/java/com/joao/projetofinal/models/Curso.kt
+++
@@ -1,3 +0,0 @@
-package com.joao.projetofinal.models
-
-data class Curso(val nome: String,val descricao: String, val disciplinas: List<Disciplina>)
